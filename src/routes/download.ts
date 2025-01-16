fc961147f84fa98b97a4d4025dbf35ab5d4eab2c
João Pedro <11joao44@gmail.com>
2025-01-16 19:13:36 -0400

    Update: adicionando cookies

--- src/routes/download.ts
+++ src/routes/download.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { spawn } from 'child_process';
 import path from 'path';
+import fs from "fs";
+import os from "os";
 
 // Definir o tipo para os parâmetros da rota
 interface DownloadQuery {
@@ -19,12 +21,25 @@ export default async function downloadRoutes(fastify: FastifyInstance): Promise<
   
     try {
       const ytDlpPath = path.resolve("./bin/yt-dlp");
-      const args = ["-o", "-", url];
-      if (format) {
-        args.splice(2, 0, "-f", format);
+
+      // Criar arquivo temporário para cookies
+      const tempDir = os.tmpdir();
+      const cookiesPath = path.join(tempDir, "cookies.txt");
+      const cookies = process.env.YOUTUBE_COOKIES;
+  
+      if (!cookies) {
+        return reply.code(500).send({ error: "Cookies não configurados no ambiente." });
       }
   
-      const process = spawn(ytDlpPath, args);
+      // Escrever os cookies no arquivo temporário
+      fs.writeFileSync(cookiesPath, cookies, "utf8");
+  
+      const args = ["--cookies", cookiesPath, "-o", "-", url];
+      if (format) {
+        args.splice(4, 0, "-f", format);
+      }
+  
+      const processyt = spawn(ytDlpPath, args);
   
       // Cabeçalhos de resposta
       reply.raw.writeHead(200, {
@@ -35,7 +50,7 @@ export default async function downloadRoutes(fastify: FastifyInstance): Promise<
       let hasData = false;
   
       // Captura saída do yt-dlp
-      process.stdout.on("data", (chunk) => {
+      processyt.stdout.on("data", (chunk) => {
         if (chunk.length > 0) {
           hasData = true;
           reply.raw.write(chunk);
@@ -44,11 +59,11 @@ export default async function downloadRoutes(fastify: FastifyInstance): Promise<
         }
       });
   
-      process.stderr.on("data", (data) => {
+      processyt.stderr.on("data", (data) => {
         console.error(`[yt-dlp error]: ${data.toString()}`);
       });
   
-      process.on("close", (code) => {
+      processyt.on("close", (code) => {
         if (code !== 0 || !hasData) {
           console.error(`Processo yt-dlp finalizado com código ${code} ou sem dados.`);
           if (!reply.raw.headersSent) {
@@ -60,7 +75,7 @@ export default async function downloadRoutes(fastify: FastifyInstance): Promise<
         }
       });
   
-      process.on("error", (err) => {
+      processyt.on("error", (err) => {
         console.error("Erro no processo yt-dlp:", err);
         if (!reply.raw.headersSent) {
           reply.code(500).send({ error: "Erro interno ao processar o download." });
