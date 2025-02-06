b036c84cef84fe2788f2e961795dce40c7197a52
João Pedro <113033970+11joao44@users.noreply.github.com>
2025-02-06 14:12:09 -0400

    Update download.ts

--- src/routes/download.ts
+++ src/routes/download.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { spawn } from "child_process";
-onst axios = require('axios');
+import axios = from "axios";
 import path from "path";
 import fs from "fs";
 import os from "os";
